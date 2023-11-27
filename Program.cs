1ad4ce3327a44024b485107db2f7e8d839483a68
Diego Katz <diekatz@gmail.com>
2023-11-27 18:09:58 -0300

    terminado y verificado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double gradosAr, radianes, pi = 3.14159;
+            Console.Write("Ingresa los grados: ");
+            gradosAr = Convert.ToDouble(Console.ReadLine());
+
+            radianes = Convertir(gradosAr);
+
+            Console.WriteLine("{0}° equivalen a {1} Radianes", gradosAr, radianes); 
+
+        }
+        static double Convertir(double gradosPa)
+        {
+            double radianes;
+
+            radianes = gradosPa * Math.PI / 180;
+            return radianes;
+
+        }
+    }
+}
